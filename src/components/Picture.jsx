e78b6035f245e11ed9282c88b502c8f72f000bc8
halvaradop_ <hernanvid123@gmail.com>
2023-05-16 11:34:02 -0500

    complete main section

---
+++ src/components/Picture.jsx
@@ -0,0 +1,12 @@
+
+
+const Picture = ({ classPicture, classImg, mobile, desktop, alt, breakpoint, onClick }) => {
+    return (
+        <picture className={classPicture}>
+            {breakpoint && <source media={`(min-width: ${breakpoint})`} srcSet={desktop} />}
+            <img className={classImg} src={mobile} alt={alt} onClick={onClick} />
+        </picture>
+    )
+}
+
+export { Picture }
